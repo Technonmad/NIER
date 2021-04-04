31ae5a49930bb825bc8438c5117665ac715039ac
Anton <65555937+Technonmad@users.noreply.github.com>
2021-04-04 17:18:46 +0300

    Переделал html в php

---
+++ thanks.php
@@ -0,0 +1,23 @@
+<html>
+    
+    <head>
+        <title> Завершение регистрации </title>
+        <meta charset="UTF-8">
+        <link href="style.css" rel="stylesheet" type="text/css">
+        <script language="javascript">
+            function moveTo() 
+            {
+                location.href = "messages.php"; /* ссылка на новую страницу */
+            }
+                window.onload=setTimeout('moveTo()', 4000); /* время задержки (в милисекундах) */
+        </script>
+    </head>
+
+    <body>
+        <div class="thx">
+            <h1>Благодарю за регистрацию.
+                <br>А теперь пошлите чатиться! :)</br>
+            </h1>
+        </div>
+    </body>
+</html>
