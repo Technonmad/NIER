a1426b9a567285541f680481593894b99d3aad12
Anton <65555937+Technonmad@users.noreply.github.com>
2021-04-04 17:18:02 +0300

    Переделал html в php

---
+++ messages.php
@@ -0,0 +1,26 @@
+<html>
+    
+    <head>
+        <title> Переписька </title>
+        <meta charset="UTF-8">
+        <link href="style.css" rel="stylesheet" type="text/css">
+    </head>
+
+    <body>
+        <div class="msg">
+            <input type="text" name="message_log">
+        </div>
+        <div id="online">
+            Люди онлайн
+        </div>
+        <div id="online1">
+            
+        </div>
+        <div class="msg">
+            <input type="text" name="message" value="Сообщение...">
+        </div>
+        <div class="msg">
+            <input type="button" value="Отправить">
+        </div>
+    </body>
+</html>
